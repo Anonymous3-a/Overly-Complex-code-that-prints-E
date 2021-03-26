2f90c0dd79217e71f4bac21f48d017b669ee97f9
ay a <doydoy.com.r@gmail.com>
2021-03-26 23:20:55 +0000

    I had it on Hello World but fixed it so it prints E

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+		int num1 = 10;
+		int num2 = -8;
+		int sum = num1 + num2;
+		if (sum == 2){
+			System.out.println("E");
+		}
   }
 }
